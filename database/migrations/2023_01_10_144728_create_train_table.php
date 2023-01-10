8ca6691e4932121ccd110e434cd6e1cafb1df587
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2023-01-10 17:02:28 +0100

    fatta migrazione

---
+++ database/migrations/2023_01_10_144728_create_train_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('train', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency', 25);
+            $table->string('departure_station', 20);
+            $table->string('arrival_station', 20);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('code_of_train', 15)->nullable();
+            $table->integer('train_car_number')->nullable();
+            $table->boolean('is_on_time')->default(1);
+            $table->boolean('is_deleted')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('train');
+    }
+};
